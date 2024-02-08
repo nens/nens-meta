de540f24db2709ad608da43bf8c570986074c81b
Reinout van Rees <reinout@vanrees.org>
2024-02-08 01:22:05 +0100

    Added utility function

---
+++ nens_meta/utils.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def write_if_changed(victim: Path, content: str):
+    """Write content to file if different, not if it is the same
+
+    And create the file if it doesn't exist.
+    """
+    if victim.exists():
+        action = "Updated"
+        existing_content = victim.read_text()
+        if content == existing_content:
+            logger.debug(f"{victim} remained the same")
+            return
+    else:
+        action = "Created"
+
+    victim.write_text(content)
+    logger.info(f"{action} {victim}")
