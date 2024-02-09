f87a90cbe6c067b65479d205d2eca2afcdbf6746
Reinout van Rees <reinout@vanrees.org>
2024-02-09 12:03:17 +0100

    Generated files with cookiecutter

---
+++ nens_meta/tests/test_scripts.py
@@ -0,0 +1,9 @@
+"""Tests for script.py"""
+from nens_meta import scripts
+
+
+def test_get_parser():
+    parser = scripts.get_parser()
+    # As a test, we just check one option. That's enough.
+    options = parser.parse_args()
+    assert options.verbose is False
